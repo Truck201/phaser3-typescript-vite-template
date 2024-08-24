f6c0e0b400288dec2df9b0201b64d37abc3cbb28
Truck201 <joaquingandini22@gmail.com>
2024-08-24 00:53:22 -0700

    colors

--- scenes/controles.js
+++ scenes/controles.js
@@ -1,4 +1,22 @@
 /* como idea, la primera vez que se inicia
 esta escena que muestre los controles para jugar,
 o que se muestre en el menú
+
+NEGRO -- #000000 
+AZUL -- #1D2B53 
+MORADO -- #7E2553 
+VERDE OSC -- #008751
+MARRÓN -- #AB5236 
+GRIS -- #5F574F 
+GRIS CL -- #C2C3C7 
+BL -- #FFF1E8 
+ROJO -- #FF004D
+NARANJA -- #FFA300
+AMARILLO -- #FFEC27 
+VERDE CL -- #00E436 
+CELESTE -- #29ADFF 
+LILA -- #83769C 
+ROSADO -- #FF77A8 
+CREMA -- #FFCCAA
+
 */
